607f37df04377362e43289000544095321a78198
Matthew Brelsford <matt@giantlightstudios.com>
2020-01-31 20:17:59 -0500

    dk play

---
+++ GGJ 2020 Unity Project/Assets/_ggj2020/Board/Board.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Tile tilePrefab;
+    public Material tile1Material;
+    public Material tile2Material;
+    public int width;
+    public int depth;
+
+    public void Generate(){
+        foreach(Transform child in transform){
+            DestroyImmediate(child.gameObject);
+        }
+        for(var x = 0; x < width; x++){
+            for(var z = 0; z < depth; z++){
+                var newTile = Instantiate(tilePrefab, transform);
+                newTile.transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+
+}
